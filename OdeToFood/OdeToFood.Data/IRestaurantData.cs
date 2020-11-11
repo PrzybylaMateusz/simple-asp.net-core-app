6d9db61ca95cd1d94c989a9ffc748e4e518e9e35
PrzybylaMateusz <mateuszprzybyla.geo@gmail.com>
2020-11-11 16:50:43 +0100

    inititial commit, created project, added restauarant list page and details page

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,40 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Marco", Location="Gdańsk", Cusine=CusineType.Italian },
+                new Restaurant { Id = 2, Name = "Kebab King", Location="Warszawa", Cusine=CusineType.Indian },
+                new Restaurant { Id = 3, Name = "Gigar", Location="Ełk", Cusine=CusineType.Mexican },
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return restaurants
+                .Where(r => string.IsNullOrEmpty(name) || r.Name.StartsWith(name))
+                .OrderBy(r => r.Name);
+        }
+    }
+}
